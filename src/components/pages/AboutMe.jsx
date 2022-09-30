a69e0beb77cd9ac21690d28adbc026aa968f8a29
Ze <zepedro4259@gmail.com>
2022-09-30 16:48:54 +0100

    Changed font in work info

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -1,12 +1,29 @@
 import ResponsiveAppBar from "../common/ResponsiveAppBar"
+import Typography from '@mui/material/Typography';
+
 
 export default function AboutMe() {
     return (
         <div>
-            <ResponsiveAppBar/>
-            <h1>
-                About Me
-            </h1>
+            <ResponsiveAppBar />
+            <Typography
+                variant="h3"
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+                sx={{
+                    mr: 3,
+                    flexGrow: 1,
+                    fontFamily: 'Roboto Slab',
+                    fontWeight: 700,
+                    color: '#2E3B55',
+                    textDecoration: 'none',
+                }}
+            >
+                Luís Jesus
+            </Typography>
         </div>
     )
 }
