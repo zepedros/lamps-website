6242742da9703442f95931b621036affdeaf4eb5
Ze <zepedro4259@gmail.com>
2022-09-30 16:19:42 +0100

    changed work info for mobile

--- src/components/pages/works/WorkInfo.jsx
+++ src/components/pages/works/WorkInfo.jsx
@@ -4,32 +4,22 @@ import Typography from '@mui/material/Typography';
 import CloseIcon from '@mui/icons-material/Close';
 import IconButton from '@mui/material/IconButton';
 import Grid from '@mui/material/Grid';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 export default function WorkInfo(props) {
 
     return (
         <div>
-            {/**
-             * <Card
-                sx={{ maxWidth: "80vw", maxHeight: "auto" }}
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    height: "100vh",//100% view height
-                    width: "100vw"
-                }}
-            >
-                
-            </Card>
-             */}
-
             <Grid container spacing={2}
                 sx={{
-                    height: "80vh",//100% view height
-                    width: "auto",  // 100% view width
+                    display: { xs: 'none', md: 'flex' }
                 }}
             >
+                <Grid item>
+                    <IconButton onClick={props.handleClosing}>
+                        <ArrowBackIcon />
+                    </IconButton>
+                </Grid>
                 <Grid item xs={11}
                     style={{
                         display: 'flex',
@@ -60,11 +50,48 @@ export default function WorkInfo(props) {
                         </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item xs={1}>
-                    <IconButton
-                        onClick={props.handleClosing}
-                    >
-                        <CloseIcon></CloseIcon>
+            </Grid>
+            <Grid container spacing={1}
+                sx={{
+                    display: { xs: 'flex', md: 'none' }
+                }}
+            >
+                <Grid item xs={12}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <CardMedia
+                        sx={{
+                            height: "auto",//100% view height
+                            width: "95vw",  // 100% view width
+                            display: { xs: 'flex', md: 'none' }
+                        }}
+                        component="img"
+                        image={props.img}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {props.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {props.description}
+                        </Typography>
+                        <Typography gutterBottom variant="body2" color="text.secondary">
+                            {"90x160cm"}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {"Oil"}
+                        </Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item xs={12}>
+                    <IconButton onClick={props.handleClosing}>
+                        <ArrowBackIcon />
                     </IconButton>
                 </Grid>
             </Grid>
