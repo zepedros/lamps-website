49258afb7e019903561d3b1df01b950d3b899ee9
zepedros <zepedro4259@gmail.com>
2023-01-14 23:47:05 +0000

    Two images now being displayed

--- src/components/pages/works/NewWorks.jsx
+++ src/components/pages/works/NewWorks.jsx
@@ -8,57 +8,10 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
-import alfamaViewImage from "../../../images/horizontal/WhatsApp Image 2022-09-08 at 17.33.31.jpeg"
-import lisbonBuildingsImage from "../../../images/horizontal/WhatsApp Image 2022-09-08 at 17.33.33.jpeg"
+import waterColor from "../../../images/horizontal/WhatsApp Image 2022-09-08 at 17.33.31.jpeg"
+import acrylicPainting from "../../../images/NOO3.jpg"
+
 import ResponsiveAppBar from '../../common/ResponsiveAppBar';
-
-export default function NewWorks() {
-    /*let box = document.querySelector('.box');
-    let width = box.offsetWidth;
-    let height = box.offsetHeight;*/
-    return (
-        /*
-        <div>
-            <ResponsiveAppBar/>
-            <ImageList sx={{width: maxWidth, height: "800px" }} cols={1} rowHeight={"auto"}>
-                {itemData.map((item) => (
-                    <ImageListItem key={item.img}>
-                        <img
-                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                            alt={item.title}
-                            loading="lazy"
-                        />
-                    </ImageListItem>
-                ))}
-            </ImageList>
-        </div>*/
-        <div
-            width="max-width"
-        >
-            <ResponsiveAppBar />
-            <Card sx={{ width: "max-width", maxHeight: getWindowDimensions().height }}>
-                <CardActionArea>
-                    <CardMedia
-                        sx={{ height: "50%" }}
-                        component="img"
-                        height="auto"
-                        image={itemData[0].img}
-                        alt="Water Colors"
-                    />
-                    <CardMedia
-                        sx={{ height: "50%" }}
-                        component="img"
-                        height="auto"
-                        image={itemData[0].img}
-                        alt="Water Colors"
-                    />
-                </CardActionArea>
-            </Card>
-        </div>
-    );
-}
-
 
 function getWindowDimensions() {
     const { innerWidth: width, innerHeight: height } = window;
@@ -83,13 +36,45 @@ function useWindowDimensions() {
     return windowDimensions;
 }
 
+
+export default function NewWorks() {
+    return (
+        <Card sx={{ width: "max-width"}}>
+            <ResponsiveAppBar/>
+                <CardActionArea
+                    sx={{ maxHeight: (getWindowDimensions().height) / 2 -36 }}
+                >
+
+                    <CardMedia
+                        component="img"
+                        height="auto"
+                        image={itemData[0].img}
+                        alt="Water Colors"
+                    />
+                </CardActionArea>
+                <CardActionArea
+                    sx={{ maxHeight: (getWindowDimensions().height) / 2 }}
+                >
+                    <CardMedia
+                        component="img"
+                        height="auto"
+                        image={itemData[1].img}
+                        alt="Water Colors"
+                    />
+                </CardActionArea>
+            </Card>
+    );
+}
+
+
+
 const itemData = [
     {
-        img: alfamaViewImage,
-        title: "Title 1"
+        img: waterColor,
+        title: "Water Colors"
     },
     {
-        img: lisbonBuildingsImage,
-        title: "Title 2"
+        img: acrylicPainting,
+        title: "Acrylics"
     }
 ]
